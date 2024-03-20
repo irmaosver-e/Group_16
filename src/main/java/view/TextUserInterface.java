386e061b1959136685082334ad55ac74f75a170d
Nani <irmaosver_e@hotmail.com>
2024-03-20 18:03:50 +0000

    lucene thing

--- src/main/java/view/TextUserInterface.java
+++ src/main/java/view/TextUserInterface.java
@@ -1,4 +1,10 @@
 package view;
 
+import model.PageSearchResult;
+
+import java.util.ArrayList;
+
 public class TextUserInterface {
+    public void displaySearchResult(ArrayList<PageSearchResult> pageResultList){
+        View.displaySearchResult(pageResultList);}
 }
--- src/main/java/view/View.java
+++ src/main/java/view/View.java
@@ -1,4 +1,9 @@
 package view;
 
+import model.PageSearchResult;
+
+import java.util.ArrayList;
+
 public class View {
+    public void displaySearchResult(ArrayList<PageSearchResult> pageResultList){}
 }
