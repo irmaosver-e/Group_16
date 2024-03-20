70c89c94931aee329c642208060add7cdd75156e
skyee <skye.emberson@gmail.com>
2024-03-20 18:17:02 +0000

    more lucene thing

--- src/main/java/view/TextUserInterface.java
+++ src/main/java/view/TextUserInterface.java
@@ -6,5 +6,6 @@ import java.util.ArrayList;
 
 public class TextUserInterface {
     public void displaySearchResult(ArrayList<PageSearchResult> pageResultList){
-        View.displaySearchResult(pageResultList);}
+        //View.displaySearchResult(pageResultList);
+        }
 }
