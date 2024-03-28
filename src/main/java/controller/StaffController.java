21a2aae1b2499b3c7e84e34307461ea199d31b1c
skyee <skye.emberson@gmail.com>
2024-03-28 16:22:05 +0000

    staff controller update

--- src/main/java/controller/StaffController.java
+++ src/main/java/controller/StaffController.java
@@ -2,12 +2,43 @@ package controller;
 
 import external.AuthenticationService;
 import external.EmailService;
+import model.Inquiry;
 import model.SharedContext;
 import view.View;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 public class StaffController extends Controller{
     public StaffController(SharedContext sharedCont, View theView, AuthenticationService authServ,
                            EmailService emailServ){
         super(sharedCont, theView,authServ,emailServ);
+    }
+
+    protected Collection<String> getInquiryTitles(Collection<Inquiry> colInquiries)
+    {
+        Collection<String> colStr = new ArrayList<>();
+        for (Inquiry inquiry : colInquiries) {
+            colStr.add(inquiry.getSubject());
+        }
+        return colStr;
+    }
+
+
+
+    protected void respondToInquiry(Inquiry inquiry){
+        //check user is logged in
+
+
+        //should it have the same subject as the inquirer email?
+        String subject=theView.getInput("Input your subject for this email.");
+
+        String content = theView.getInput("Input the content of this email.");
+
+        emailServ.sendEmail(currentUser.email,
+                inquiry.getInquirerEmail(), subject, content);
+        //system has to confirm the email was sent
+
+        //the inquiry needs to be deleted from the system
     }
 }
