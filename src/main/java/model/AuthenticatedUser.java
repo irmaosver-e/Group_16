d7e2015abb570a947ca591d91cedd49f156f7352
Nani <irmaosver_e@hotmail.com>
2024-03-31 17:20:49 +0100

    main login

--- src/main/java/model/AuthenticatedUser.java
+++ src/main/java/model/AuthenticatedUser.java
@@ -3,7 +3,10 @@ package model;
 public class AuthenticatedUser extends User {
     private String email;
     private String role;
-    public AuthenticatedUser(String email, String role){}
+    public AuthenticatedUser(String email, String role){
+        this.email = email;
+        this.role = role;
+    }
 
     public String getEmail() {
         return email;
