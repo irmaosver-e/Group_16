1691d0707508e1ee424aea5f17e7db6ea241095d
ishaniraj <ishaniraj30@gmail.com>
2024-03-31 19:33:13 +0100

    MenuController completed

--- src/main/java/controller/AdminStaffMainMenuOption.java
+++ src/main/java/controller/AdminStaffMainMenuOption.java
@@ -4,7 +4,9 @@ public enum AdminStaffMainMenuOption {
     LOGOUT(0),
     MANAGE_QUERIES(1),
     ADD_PAGE(2),
-    MANAGE_FAQ(3);
+
+    SEE_ALL_PAGES(3),
+    MANAGE_FAQ(4);
     public final int value;
 
     AdminStaffMainMenuOption(int v) {
--- src/main/java/controller/MenuController.java
+++ src/main/java/controller/MenuController.java
@@ -43,35 +43,79 @@ public class MenuController extends Controller {
     }
 
     private boolean handleGuestMainMenu(){
-        GuestController guest = new GuestController(this.sharedCont,this.theView,this.authServ,this.emailServ);
-
         this.theView.displayInfo("Guest Menu:");
-        try{
-            int index = 0;
-            EnumSet<GuestMainMenuOption> menuOptions = EnumSet.allOf(GuestMainMenuOption.class);
-            for(GuestMainMenuOption option : menuOptions){
-                this.theView.displayInfo("input ["+index+"] for: "+menuOptions);
+        int index = 0;
+        EnumSet<GuestMainMenuOption> menuOptions = EnumSet.allOf(GuestMainMenuOption.class);
+        for (GuestMainMenuOption option : menuOptions) {
+            this.theView.displayInfo("input [" + index + "] for: " + option.toString().replace("_", " "));
+            index++;
+        }
+        String input = theView.getInput("Please make a selection: ");
+        try {
+            int choice = Integer.parseInt(input);
+            if (choice >= 0 && choice < menuOptions.size()) {
+                GuestMainMenuOption selectedOption = (GuestMainMenuOption) menuOptions.toArray()[choice];
+                switch (selectedOption) {
+                    case LOGIN:
+                        guestCtrler.login();
+                        break;
+                    case CONSULT_FAQ:
+                        inquirerCtrler.consultFAQ();
+                        break;
+                    case SEARCH_PAGES:
+                        inquirerCtrler.searchPages();
+                        break;
+                    case CONTACT_STAFF:
+                        inquirerCtrler.contactStaff();
+                        break;
+                    default:
+                        theView.displayError("Unknown selection.");
+                        break;
+                }
+            } else {
+                theView.displayError("Invalid selection. Please try again.");
             }
-
-
-        }catch (NumberFormatException e){
-            this.theView.displayException(e);
+        } catch (NumberFormatException e) {
+            this.theView.displayError("Invalid input. Please enter a number.");
         }
         return true;
     }
 
     private boolean handleStudentMainMenu(){
         this.theView.displayInfo("Student Menu:");
-        try{
-            int index = 0;
-            EnumSet<TeachingStaffMainMenuOption> menuOptions = EnumSet.allOf(TeachingStaffMainMenuOption.class);
-            for(TeachingStaffMainMenuOption option : menuOptions){
-                this.theView.displayInfo("input ["+index+"] for: "+menuOptions);
+        int index = 0;
+        EnumSet<StudentMainMenuOption> menuOptions = EnumSet.allOf(StudentMainMenuOption.class);
+        for (StudentMainMenuOption option : menuOptions) {
+            this.theView.displayInfo("input [" + index + "] for: " + option.toString().replace("_", " "));
+            index++;
+        }
+        String input = theView.getInput("Please make a selection: ");
+        try {
+            int choice = Integer.parseInt(input);
+            if (choice >= 0 && choice < menuOptions.size()) {
+                StudentMainMenuOption selectedOption = (StudentMainMenuOption) menuOptions.toArray()[choice];
+                switch (selectedOption) {
+                    case LOGOUT:
+                        authUserCtrler.logout();
+                        break;
+                    case CONSULT_FAQ:
+                        inquirerCtrler.consultFAQ();
+                        break;
+                    case SEARCH_PAGES:
+                        inquirerCtrler.searchPages();
+                        break;
+                    case CONTACT_STAFF:
+                        inquirerCtrler.contactStaff();
+                        break;
+                    default:
+                        theView.displayError("Unknown selection.");
+                        break;
+                }
+            } else {
+                theView.displayError("Invalid selection. Please try again.");
             }
-
-
-        }catch (NumberFormatException e){
-            this.theView.displayException(e);
+        } catch (NumberFormatException e) {
+            this.theView.displayError("Invalid input. Please enter a number.");
         }
         return true;
     }
@@ -80,26 +124,31 @@ public class MenuController extends Controller {
         this.theView.displayInfo("Teaching Staff Menu:");
         int index = 0;
         EnumSet<TeachingStaffMainMenuOption> menuOptions = EnumSet.allOf(TeachingStaffMainMenuOption.class);
-        for(TeachingStaffMainMenuOption option : menuOptions){
-            this.theView.displayInfo("input ["+index+"] for: "+menuOptions);
+        for (TeachingStaffMainMenuOption option : menuOptions) {
+            this.theView.displayInfo("input [" + index + "] for: " + option.toString().replace("_", " "));
+            index++;
         }
-        String input = theView.getInput("Please make a selection:");
-        try{
+        String input = theView.getInput("Please make a selection: ");
+        try {
             int choice = Integer.parseInt(input);
-            if (choice >= 0 && choice < menuOptions.size()){
-                TeachingStaffMainMenuOption selectedOption =
-                        (TeachingStaffMainMenuOption) menuOptions.toArray()[choice];
-                switch(choice){
-                    case 0:
+            if (choice >= 0 && choice < menuOptions.size()) {
+                TeachingStaffMainMenuOption selectedOption = (TeachingStaffMainMenuOption) menuOptions.toArray()[choice];
+                switch (selectedOption) {
+                    case LOGOUT:
                         authUserCtrler.logout();
                         break;
-                    case 1:
+                    case MANAGE_RECEIVED_QUERIES:
                         teachingStaffCtrler.manageReceivedInquiries(sharedCont.getUnAnsweredInquiries());
                         break;
+                    default:
+                        theView.displayError("Unknown selection.");
+                        break;
                 }
+            } else {
+                theView.displayError("Invalid selection. Please try again.");
             }
-        }catch (NumberFormatException e){
-            this.theView.displayException(e);
+        } catch (NumberFormatException e) {
+            this.theView.displayError("Invalid input. Please enter a number.");
         }
         return true;
     }
@@ -108,38 +157,40 @@ public class MenuController extends Controller {
         this.theView.displayInfo("Admin Staff Menu:");
         int index = 0;
         EnumSet<AdminStaffMainMenuOption> menuOptions = EnumSet.allOf(AdminStaffMainMenuOption.class);
-        for(AdminStaffMainMenuOption option : menuOptions){
-            this.theView.displayInfo("input ["+index+"] for: "+menuOptions);
+        for (AdminStaffMainMenuOption option : menuOptions) {
+            this.theView.displayInfo("input [" + index + "] for: " + option.toString().replace("_", " "));
+            index++;
         }
-        String input = theView.getInput("Please make a selection:");
-        try{
+        String input = theView.getInput("Please make a selection: ");
+        try {
             int choice = Integer.parseInt(input);
-            if (choice >= 0 && choice < menuOptions.size()){
-                AdminStaffMainMenuOption selectedOption =
-                        (AdminStaffMainMenuOption) menuOptions.toArray()[choice];
-                switch(choice){
-                    case 0:
+            if (choice >= 0 && choice < menuOptions.size()) {
+                AdminStaffMainMenuOption selectedOption = (AdminStaffMainMenuOption) menuOptions.toArray()[choice];
+                switch (selectedOption) {
+                    case LOGOUT:
                         authUserCtrler.logout();
                         break;
-                    case 1:
+                    case MANAGE_QUERIES:
                         adminStaffCtrler.manageInquiries(sharedCont.getUnAnsweredInquiries());
                         break;
-                    case 2:
+                    case ADD_PAGE:
                         adminStaffCtrler.addPage();
                         break;
-                    case 3:
+                    case SEE_ALL_PAGES:
                         adminStaffCtrler.viewAllPages();
                         break;
-                    case 4:
+                    case MANAGE_FAQ:
                         adminStaffCtrler.manageFAQ();
                         break;
+                    default:
+                        theView.displayError("Unknown selection.");
+                        break;
                 }
+            } else {
+                theView.displayError("Invalid selection. Please try again.");
             }
-
-
-
-        }catch (NumberFormatException e){
-            this.theView.displayException(e);
+        } catch (NumberFormatException e) {
+            this.theView.displayError("Invalid input. Please enter a number.");
         }
         return true;
     }
