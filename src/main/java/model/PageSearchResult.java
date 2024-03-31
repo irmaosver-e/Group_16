964aaa164ce910036fbf2132dc1fe8fb7b88c8f2
Nani <irmaosver_e@hotmail.com>
2024-03-31 14:57:07 +0100

    Lucene implemented

--- src/main/java/model/PageSearchResult.java
+++ src/main/java/model/PageSearchResult.java
@@ -1,9 +1,16 @@
 package model;
 
+// Class representing a search result from a page search
 public class PageSearchResult {
-    private String formattedContent;
+    private String formattedContent; // Formatted content of the search result
 
-    public PageSearchResult(String rawResult){this.formattedContent = rawResult;}
+    // Constructor to initialize a PageSearchResult object with rawResult as formatted content
+    public PageSearchResult(String rawResult) {
+        this.formattedContent = rawResult;
+    }
 
-    public String getFormattedContent(){return formattedContent;}
+    // Getter method to retrieve the formatted content of the search result
+    public String getFormattedContent() {
+        return formattedContent;
+    }
 }
