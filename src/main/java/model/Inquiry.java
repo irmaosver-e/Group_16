27b5d9bd2f87218840318fbc853c8b132f69c725
piggy-mad <148875780+piggy-mad@users.noreply.github.com>
2024-04-05 04:30:07 +0100

    Add files via upload

--- src/main/java/model/Inquiry.java
+++ src/main/java/model/Inquiry.java
@@ -1,31 +1,6 @@
 package model;
 
-import java.time.DateTimeException;
-
 public class Inquiry {
-
-    private String subject;
-
-    private DateTimeException createdAt;
-
-    private String inquirerEmail;
-
-    private String content;
-
-    private String assignedTo;
-
-    public Inquiry (String inquirerEmail, String subject, String content){
-        this.inquirerEmail = inquirerEmail;
-        this.subject = subject;
-        this.content = content;
-    }
-
-    public String getSubject(){return subject; }
-    public String getInquirerEmail(){
-        return inquirerEmail;
-    }
-
-    public String getEmailContent() {
-        return content;
+    public Inquiry(String inquirerEmail, String subject, String content) {
     }
 }
