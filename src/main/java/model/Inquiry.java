aedeb1b84987bb47660a216aa0822e3138e9b8e1
ishaniraj <ishaniraj30@gmail.com>
2024-04-06 18:56:56 +0100

    Inquiry.java completed

--- src/main/java/model/Inquiry.java
+++ src/main/java/model/Inquiry.java
@@ -1,12 +1,20 @@
 package model;
 
 public class Inquiry {
+
+    private String inquirerEmail;
+    private String subject;
+    private String content;
+
     public Inquiry(String inquirerEmail, String subject, String content) {
+        this.inquirerEmail = inquirerEmail;
+        this.subject = subject;
+        this.content = content;
     }
 
-    public String getSubject() {return "place holder";}
+    public String getSubject() {return subject;}
 
-    public String getInquirerEmail() { return "mock email";}
+    public String getInquirerEmail() { return inquirerEmail;}
 
-    public String getEmailContent() { return "email content";}
+    public String getEmailContent() { return content;}
 }
